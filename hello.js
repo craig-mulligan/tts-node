90fede46c5938b40b6ae4279c2e20032e5db6681
Craig Mulligan <craig@firingsuad.co.za>
2015-04-30 15:16:34 +0100

    responsive

--- hello.js
+++ hello.js
@@ -9,13 +9,15 @@ require('http').createServer(function (req, res) {
         tts.speak(str, 'en');
 	});
 
-	res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-	res.write('<html><head><meta name="viewport" content="width=device-width, initial-scale=1"><title>Resin.io Text2Speech</title></head><body style="text-align:center;">');
-	res.write('<h1>Resin.io Text2Speech</h1>');
-	res.write('<form enctype="application/x-www-form-urlencoded" action="/" method="post">');
-	res.write('<input type="text" name="text" value="" placeholder="Speak this text"/><br /><br />');
-	res.write('<input type="submit" />');
-	res.write('</form></body></html');
-	res.end();
+	req.on('end', function () {
+
+		res.write('<html><head><meta name="viewport" content="width=device-width, initial-scale=1"><title>Resin.io Text2Speech</title></head><body style="text-align:center;">');
+		res.write('<h1>Resin.io Text2Speech</h1>');
+		res.write('<form enctype="application/x-www-form-urlencoded" action="/" method="post">');
+		res.write('<input type="text" name="text" value="" placeholder="Speak this text"/><br /><br />');
+		res.write('<input type="submit" />');
+		res.write('</form></body></html');
+		res.end();
+    });
 
 }).listen(8080);
