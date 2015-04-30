9e947628d6d049c99255550e41c92c27b5d6282e
Craig Mulligan <craig@firingsuad.co.za>
2015-04-30 12:18:50 +0100

    add browser form

--- hello.js
+++ hello.js
@@ -2,15 +2,21 @@ var tts = require("node-tts-google").tts;
 
 require('http').createServer(function (req, res) {
 
-    req.on('data', function (chunk) {
-        // pipe request body to speaker
-        tts.speak(chunk, 'en');
-    });
+	if (req.method === "POST") {
+		req.on('data', function (chunk) {
+	        // pipe request body to speaker
+	        str = chunk.slice(5);
+	        tts.speak(str, 'en');
+    	});
+	}
 
-    req.on('end', function () {
-        // close connection
-        res.end();
-    });
+	res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+	res.write('<html><head><title>Resin.io Text2Speech</title></head><body style="text-align:center;">');
+	res.write('<h1>Resin.io Text2Speech</h1>');
+	res.write('<form enctype="application/x-www-form-urlencoded" action="/" method="post">');
+	res.write('<input type="text" name="text" value="" placeholder="Speak this text"/><br /><br />');
+	res.write('<input type="submit" />');
+	res.write('</form></body></html');
+	res.end();
 
-	// console.log(req.query)
 }).listen(8080);
