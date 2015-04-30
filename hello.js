292b5be1638d0184e53537e2932c4697ae3d9a92
Craig Mulligan <craig@firingsuad.co.za>
2015-04-30 14:10:08 +0100

    responsive

--- hello.js
+++ hello.js
@@ -2,17 +2,15 @@ var tts = require("node-tts-google").tts;
 
 require('http').createServer(function (req, res) {
 
-	if (req.method === "POST") {
-		req.on('data', function (chunk) {
-	        // pipe request body to speaker
-	        str = chunk.slice(5);
-	        console.log(str);
-	        tts.speak(str, 'en');
-    	});
-	}
+	req.on('data', function (chunk) {
+        // pipe request body to speaker
+        str = chunk.slice(5);
+        console.log(str);
+        tts.speak(str, 'en');
+	});
 
 	res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-	res.write('<html><head><title>Resin.io Text2Speech</title></head><body style="text-align:center;">');
+	res.write('<html><head><meta name="viewport" content="width=device-width, initial-scale=1"><title>Resin.io Text2Speech</title></head><body style="text-align:center;">');
 	res.write('<h1>Resin.io Text2Speech</h1>');
 	res.write('<form enctype="application/x-www-form-urlencoded" action="/" method="post">');
 	res.write('<input type="text" name="text" value="" placeholder="Speak this text"/><br /><br />');
