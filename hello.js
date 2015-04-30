80cda9c780f0f20b46d79e107c335632fa8ca684
Craig Mulligan <craig@firingsuad.co.za>
2015-04-30 13:44:05 +0100

    add browser form

--- hello.js
+++ hello.js
@@ -10,6 +10,8 @@ require('http').createServer(function (req, res) {
     	});
 	}
 
+	console.log(req.method);
+
 	res.writeHead(200, "OK", {'Content-Type': 'text/html'});
 	res.write('<html><head><title>Resin.io Text2Speech</title></head><body style="text-align:center;">');
 	res.write('<h1>Resin.io Text2Speech</h1>');
